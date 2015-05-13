9c42188cb90f55c6b76c0620ca936e776657d125
Evan Turner <evturn@gmail.com>
2015-05-13 00:28:15 -0400

    login form created

--- routes/routes.js
+++ routes/routes.js
@@ -3,8 +3,11 @@ var app = express();
 var router = express.Router();
 
 router.get('/', function(req, res){
-    res.render('chatroom/layout');
+    res.render('chatroom/index');
   });
 
+router.get('/welcome', function(req, res){
+    res.render('authentication/index');
+  });
 
 module.exports = router;
