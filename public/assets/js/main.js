583b3bd1c4cd4310fb6de9a92f2b57f4d08ef642
Evan Turner <evturn@gmail.com>
2015-05-13 21:10:49 -0400

    user being tagged in message

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -19,22 +19,24 @@ ref.onAuth(function(authData) {
   }
 });
 
-usersRef.on("value", function(snapshot) {
-  console.log(snapshot.val());
-}, function (errorObject) {
-  console.log("The read failed: " + errorObject.code);
-});
-
-
+function requestUser() {
+	ref.getAuth(function(authData) {
+	  if (authData) {
+	    console.log("Authenticated with uid:", authData.password['email']);
+	    var email = authData.password['email'];
+	    identifyUser(email);
+	  } else {
+	    console.log("Client unauthenticated.")
+	  }
+	});	
+}
 
 function identifyUser(email) {
 	usersRef.orderByChild("email").on("child_added", function(snapshot) {
-  	// console.log(snapshot.key() + " is " + snapshot.val().email + ".");
   	if (email === snapshot.val().email) {
   		console.log(email + ' is logged in');
   		syncUser(snapshot.key());
   	} 
-  
 	});
 }
 
@@ -43,6 +45,10 @@ function syncUser(username) {
 	var userRef = new Firebase(path);
 	userRef.on('value', function(snapshot) {
 		var currentUser = snapshot.val();
-		console.log(currentUser);
+		broadcastUser(username);
 	});
 };
+
+function broadcastUser(user) {
+	console.log(user + ' is sending a message');
+}
--- public/assets/js/messages.js
+++ public/assets/js/messages.js
@@ -1,7 +1,17 @@
+$(function() {
 
+	$(document).on('keypress', '.message-input', function(e) {
+		e.preventDefault();
+		if (e.keyCode === 13) {
+			sendMessage();
+		}
+	})
+
+
+});
 
 function sendMessage() {
-
+	requestUser();
 	var $input = $('.message-input').val();
 	var messagesRef = ref.child('messages');
 
